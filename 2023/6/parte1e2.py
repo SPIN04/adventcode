4a53812b81f0694413f3049bedae17bf54ae59c3
SPINO <edoardo3spinelli@gmail.com>
2023-12-06 14:38:49 +0100

    adventcode

--- 2023/6/parte1e2.py
+++ 2023/6/parte1e2.py
@@ -1,4 +1,4 @@
-# Apre il file in modalità lettura
+# il file di input è lo stesso tenere gli spazi tra i : e i dati
 new=[]
 time=[]
 record=[]
@@ -17,11 +17,20 @@ with open('2023/6/a.txt', 'r') as file:
             time.append(cella)
         else:
             record.append(cella)
-    print(time,'\n',record)
     for i, val in enumerate(time):
+        count=0
         tmz=0
         tempodisposizione=int(val)
+        tmxclone=0
+        tempodisposizioneclone=0
         while tmz*tempodisposizione<=int(record[i]):
             tmz+=1
             tempodisposizione-=1
-        print(tmz,tempodisposizione,tmz*tempodisposizione)
+            tmxclone=tmz
+            tempodisposizioneclone=tempodisposizione
+            while tmxclone*tempodisposizioneclone>int(record[i]):
+                tmxclone+=1
+                tempodisposizioneclone-=1
+                count+=1
+            
+        print(count,i)
